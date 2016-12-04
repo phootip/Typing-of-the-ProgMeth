7f5136177dab0ea551038eb2bdca7e6eea093212
DESKTOP-LVN60EJ\computer <top10_tip@hotmail.com>
2016-12-04 16:49:12 +0700

    have to create drawing utility

---
+++ src/model/InputHolder.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+
+public class InputHolder {
+	//hold input value
+	public static double mouseX, mouseY;
+	public static boolean mouseLeftDown,mouseOnScreen;
+	public static boolean mouseLeftDownTrigger;
+	public static ArrayList<String> keyPressed = new ArrayList<>();
+	public static ArrayList<String> keyTriggered = new ArrayList<>();
+}
--- src/ui/MenuScreen.java
+++ src/ui/MenuScreen.java
@@ -3,12 +3,15 @@ package ui;
 import com.sun.javafx.tk.FontLoader;
 import com.sun.javafx.tk.Toolkit;
 
+import javafx.event.EventHandler;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import model.InputHolder;
 
 public class MenuScreen extends StackPane{
 	
@@ -19,6 +22,8 @@ public class MenuScreen extends StackPane{
 	public MenuScreen(){
 		this.canvas = new Canvas(ConfigOption.width,ConfigOption.height);
 		gc = this.canvas.getGraphicsContext2D();
+		
+		//BackGround
 		gc.setFill(Color.BLACK);
 		gc.fillRect(0, 0, ConfigOption.width, ConfigOption.height);
 		
@@ -28,10 +33,56 @@ public class MenuScreen extends StackPane{
 	}
 	
 	private void paintHomeMenu(){
+		//Paint Home Menu
 		gc.setFont(font);
 		this.gc.setFill(Color.WHITE);
 		FontLoader fontLoader = Toolkit.getToolkit().getFontLoader();
+		
 		double font_width = fontLoader.computeStringWidth("START", gc.getFont());
-		gc.fillText("START", ConfigOption.width/2-font_width/2, 150);
+		gc.fillText("START", ConfigOption.width/2-font_width/2, ConfigOption.height/2);
+
+		font_width = fontLoader.computeStringWidth("OPTION", gc.getFont());
+		gc.fillText("OPTION", ConfigOption.width/2-font_width/2, ConfigOption.height/2+100);		
+		
+		font_width = fontLoader.computeStringWidth("Exit", gc.getFont());
+		gc.fillText("Exit", ConfigOption.width/2-font_width/2, ConfigOption.height/2+200);
+		
+		//Event Handler Hovering Menu
+		this.canvas.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				// TODO Auto-generated method stub
+				InputHolder.mouseOnScreen = true;
+			}
+		});
+		
+		this.canvas.setOnMouseExited(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event){
+				InputHolder.mouseOnScreen = false;
+			}
+		});
+		
+		this.canvas.setOnMouseMoved(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event){
+				if(InputHolder.mouseOnScreen){
+					InputHolder.mouseX = event.getX();
+					InputHolder.mouseY = event.getY();
+					if()
+				}
+			}
+		});
+		
+		this.canvas.setOnMouseDragged(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event){
+				if(InputHolder.mouseOnScreen){
+					InputHolder.mouseX = event.getX();
+					InputHolder.mouseY = event.getY();
+					
+				}
+			}
+		});
 	}
 }
